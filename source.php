c908c8f97060b0b0613e3e713b5667e833049afd
Stef van Dijk <stef@vandijkstef.nl>
2018-04-09 22:34:33 +0200

    Add shopping cart icon

--- source.php
+++ source.php
@@ -11,30 +11,41 @@
 		<script src="script.js" type="module"></script>
 	</head>
 	<body>
-		<main>
+		<header>
+			<div>
+				<a href="#store"><?php echo file_get_contents('./icons/svg/' . 'coffee-shop' . '.svg'); ?></a>
+			</div>
 			<h1>Beans on demand</h1>
-			<!-- <section id="filter" class="products">
-				<h3>Filter</h3>
-			</section> -->
-			<section id="products">
-				<?php
-					RenderProduct('Coffee', 'Straight up caffination', 'coffee-cup', 'coffee');
-					RenderProduct('Cappuccino', 'A milky sensation', 'cappuccino', 'coffee');
-					RenderProduct('Latte', 'Some coffee, more milk', 'latte', 'coffee');
-					RenderProduct('Espresso', 'Saving water since 1900', 'espresso', 'coffee');
-					RenderProduct('Iced Coffee', 'Hot Coffee, but cold', 'iced-coffee-1', 'coffee');
-					RenderProduct('Mocha Coffee', 'When just coffee doesn\'t do it', 'mocha', 'coffee');
-					RenderProduct('Irish Coffee', 'Unsure if sleepy', 'irish-coffee', 'coffee');
-					RenderProduct('Hot Chocolate', 'Cacaoffee', 'chocolate', 'drinks');
-					RenderProduct('Tea', 'Herbal Coffee', 'tea-cup-3', 'drinks');
-					RenderProduct('Water', 'Coffee without coffee', 'glass-of-water', 'drinks');
-					RenderProduct('Milk', 'Moo!', 'milk', 'drinks');
-					RenderProduct('Cookie Chocolate', 'Om nom nom', 'cookie', 'foods');
-					RenderProduct('Cookie Murican', 'Freedom in dough', 'cookie-1', 'foods');
-					RenderProduct('Doughnut', 'You\'re nuts!', 'doughnut', 'foods');
-					RenderProduct('Muffin', 'Like a cupcake', 'muffin', 'foods');
-					RenderProduct('Toast', 'Baked Bread', 'toast', 'foods');
-				?>
+			<div>
+				<a href="#cart"><?php echo file_get_contents('./icons/svg/' . 'shopping-cart' . '.svg'); ?></a>
+			</div>
+		</header>
+		<main>
+			<section id="store">
+
+				<!-- <section id="filter" class="products">
+					<h3>Filter</h3>
+				</section> -->
+				<section id="products">
+					<?php
+						RenderProduct('Coffee', 'Straight up caffination', 'coffee-cup', 'coffee');
+						RenderProduct('Cappuccino', 'A milky sensation', 'cappuccino', 'coffee');
+						RenderProduct('Latte', 'Some coffee, more milk', 'latte', 'coffee');
+						RenderProduct('Espresso', 'Saving water since 1900', 'espresso', 'coffee');
+						RenderProduct('Iced Coffee', 'Hot Coffee, but cold', 'iced-coffee-1', 'coffee');
+						RenderProduct('Mocha Coffee', 'When just coffee doesn\'t do it', 'mocha', 'coffee');
+						RenderProduct('Irish Coffee', 'Unsure if sleepy', 'irish-coffee', 'coffee');
+						RenderProduct('Hot Chocolate', 'Cacaoffee', 'chocolate', 'drinks');
+						RenderProduct('Tea', 'Herbal Coffee', 'tea-cup-3', 'drinks');
+						RenderProduct('Water', 'Coffee without coffee', 'glass-of-water', 'drinks');
+						RenderProduct('Milk', 'Moo!', 'milk', 'drinks');
+						RenderProduct('Cookie Chocolate', 'Om nom nom', 'cookie', 'foods');
+						RenderProduct('Cookie Murican', 'Freedom in dough', 'cookie-1', 'foods');
+						RenderProduct('Doughnut', 'You\'re nuts!', 'doughnut', 'foods');
+						RenderProduct('Muffin', 'Like a cupcake', 'muffin', 'foods');
+						RenderProduct('Toast', 'Baked Bread', 'toast', 'foods');
+					?>
+				</section>
 			</section>
 			<section id="cart">
 				<h1>Shopping Cart</h1>
