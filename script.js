1e96cf77aafad24decc8efccea88786262999d76
Stef van Dijk <stef@vandijkstef.nl>
2018-04-13 00:41:22 +0200

    updates

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const log = console.log;
 
 	const InitShoppingCart = () => {
 		cart = document.querySelector('#cart #contents');
-		document.querySelector('#cart > button').addEventListener('click', SubmitCart);
+		document.querySelector('#order').addEventListener('click', SubmitCart);
 	};
 
 	const SetProductButtons = () => {
@@ -49,22 +49,49 @@ const log = console.log;
 			// .. else render product in cart
 			RenderInCart(product);
 		}
+
+		// Tell the button to act happy
+		e.target.classList.add('succes');
+		setTimeout(() => {
+			e.target.classList.remove('succes');
+		}, 500);
+		// And the cart
+		if (!elements.cart) {
+			elements.cart = document.querySelector('a#cart');
+			console.log(elements.cart);
+		}
+		elements.cart.classList.add('shake');
+		setTimeout(() => {
+			elements.cart.classList.remove('shake');
+		}, 500);
 	};
 
 	const RenderInCart = (product) => {
 		const content = [];
+		const contentSide = [];
+
+		// Delete
+		const buttonX = UI.getButton('❌');
+		const buttonDelete = UI.getButton('🗑️', 'hidden');
+		buttonX.addEventListener('click', () => {
+			if (buttonDelete.classList.contains('hidden')) {
+				buttonDelete.classList.remove('hidden');
+				buttonX.parentElement.parentElement.parentElement.classList.add('deleting');
+			} else {
+				buttonDelete.classList.add('hidden');
+				buttonX.parentElement.parentElement.parentElement.classList.remove('deleting');
+			}
+		});
+		buttonDelete.addEventListener('click', (e) => {
+			DeleteFromCart(e.target.parentElement.parentElement);
+		});
+		content.push(UI.wrap([buttonX, buttonDelete]));
 
 		// Image
 		const productSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 		productSVG.innerHTML = product.svg;
 		content.push(UI.wrap(productSVG));
-
-		// Product name
-		content.push(UI.wrap(product.title));
-
-		// Description - TODO: Do I need this here?
-		content.push(UI.wrap(product.description));
-
+		
 		// Amount (and interactions)
 		const buttonSub = UI.getButton('-', 'sub');
 		buttonSub.addEventListener('click', (e) => {
@@ -86,28 +113,25 @@ const log = console.log;
 		});
 		content.push(UI.wrap([buttonSub, UI.getText('1', 'amount'), buttonAdd]));
 
+		// Product name
+		contentSide.push(UI.wrap(product.title));
+		
 		// Price
-		content.push(UI.wrap(product.price));
-			
-		// Delete
-		const buttonX = UI.getButton('❌');
-		const buttonDelete = UI.getButton('🗑️', 'hidden');
-		buttonX.addEventListener('click', () => {
-			buttonDelete.classList.remove('hidden');
-			buttonX.classList.add('hidden');
-		});
-		buttonDelete.addEventListener('click', (e) => {
-			DeleteFromCart(e.target.parentElement.parentElement);
-		});
-		content.push(UI.wrap([buttonX, buttonDelete]));
+		contentSide.push(UI.wrap(product.price));
 
-		UI.renderIn(content, cart, 'cart-item', 'p-' + product.id);
+		// // Description - TODO: Do I need this here?
+		// content.push(UI.wrap(product.description));
+
+
+
+		UI.renderIn([UI.wrap(content), UI.wrap(contentSide, '', '', 'aside')], cart, 'cart-item', 'p-' + product.id);
 	};
 
 	const DeleteFromCart = (row) => {
 		const parent = row.parentElement;
-		parent.removeChild(row);
-		if (parent.childElementCount === 0) {
+		const cart = parent.parentElement;
+		cart.removeChild(parent);
+		if (cart.childElementCount === 0) {
 			elements.storeLink.click();
 		}
 	};
@@ -130,8 +154,11 @@ const log = console.log;
 					elements.cartSection.classList.add('hidden');
 				});
 				elements.cartLink.addEventListener('click', () => {
-					elements.storeSection.classList.add('hidden');
 					elements.cartSection.classList.remove('hidden');
+					scrollTo(0,0);
+					setTimeout(() => {
+						elements.storeSection.classList.add('hidden');
+					}, 400);
 				});
 
 				elements.storeLink.click();
